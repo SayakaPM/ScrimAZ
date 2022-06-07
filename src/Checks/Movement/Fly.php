e4fdcf08c0d53bc655e8259703763beac0f683e0
Jaiden <103137319+ScrimBase@users.noreply.github.com>
2022-06-07 22:53:40 +0000

    Dev

---
+++ src/Checks/Movement/Fly.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace OnlyJaiden\ScrimAS\Checks\Movement;
+
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerMoveEvent;
+use pocketmine\Server;
+use pocketmine\player\Player;
+use OnlyJaiden\ScrimAS\Alert;
+
+class Fly implements Listener{
+  $report = new Alert;
+  
+    public function onPlayerMove(PlayerMoveEvent $event): void{
+      
+    }
+}
