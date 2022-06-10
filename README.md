78138594669c0ae623ff025aadd18d9c579de997
Jaiden <103137319+ScrimBase@users.noreply.github.com>
2022-06-10 23:38:37 +0100

    0.6.0

--- README.md
+++ README.md
@@ -1,9 +1,7 @@
 # ScrimAZ
 [![](https://poggit.pmmp.io/shield.state/ScrimAS)](https://poggit.pmmp.io/p/ScrimAS)
-
+\
 ScrimAZ is an anti-cheat made to find hackers.\
-If you use the code from the plugin, please credit it and don't claim the owner.\
-You can't fork my project and release it to PocketMine.\
 © 2022, Jaiden
 
 # Functionality
--- src/Checks/Movement/Fly.php
+++ src/Checks/Movement/Fly.php
@@ -3,29 +3,24 @@
 namespace OnlyJaiden\ScrimAS\Checks\Movement;
 
 use pocketmine\event\Listener;
+use pocketmine\math\Vector3;
 use pocketmine\event\player\PlayerMoveEvent;
 use pocketmine\Server;
 use pocketmine\player\Player;
 use OnlyJaiden\ScrimAS\Alert;
 
 class Fly implements Listener{
-  
     public function onPlayerMove(PlayerMoveEvent $event): void{
+      # Report is use for 
       $report = new Alert;
       $player = $event->getPlayer();
       $Oldy = $event->getFrom()->getY();
       $Newy = $event->getTo()->getY();
-      if($player->getAllowFlight() == false){
-          if($Oldy <= $Newy){
-              if($player->GetInAirTicks() > 20){
-                  $maxY = $player->getWorld()->getHighestBlockAt(floor($event->getTo()->getX()), floor($event->getTo()->getZ()));
-                  if($Newy - 2 > $maxY){
-                      $report->alert("Fly", $player->getName());
-                  }
-              }
-              
-          }
+      $blockAbove = $player->getWorld()->getBlock(new Vector3($player->getPosition()->getX(), $player->getPosition()->getY() + 1, $player->getPosition()->getZ()));
+      $predictMove = (($blockAbove ? 0 : $Oldy) - 0.08) * 0.98000001907349;
+      $difference = abs($Newy - $predictMove);
+      if($difference >  0.015){
+        $report->alert("Fly A", $player->getName());
       }
-      
     }
 }
