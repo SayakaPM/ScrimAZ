78fe323410c53dcb7ebdd620ef733c4abcc59bc0
Sayaka <103137319+SayakaPM@users.noreply.github.com>
2022-06-11 10:17:38 +0100

    tets

--- src/Checks/Movement/Fly.php
+++ src/Checks/Movement/Fly.php
@@ -3,27 +3,28 @@
 namespace OnlyJaiden\ScrimAS\Checks\Movement;
 
 use pocketmine\event\Listener;
-use pocketmine\math\Vector3;
 use pocketmine\event\player\PlayerMoveEvent;
 use pocketmine\Server;
 use pocketmine\player\Player;
 use OnlyJaiden\ScrimAS\Alert;
 
 class Fly implements Listener{
+  
     public function onPlayerMove(PlayerMoveEvent $event): void{
-      # Report is use for 
       $report = new Alert;
       $player = $event->getPlayer();
-      $blockAbove = $player->getWorld()->getBlock(new Vector3($player->getPosition()->getX(), $player->getPosition()->getY() + 1, $player->getPosition()->getZ()));
-      $movement = ($event->getFrom()->getY() - 0.08 )* 0.98000001907349;
-      $difference = abs($event->getTo()->getY() - $movement);
-      if($difference > 0.015){
-        if($player->GetInAirTicks() > 20){
-          $maxY = $player->getWorld()->getHighestBlockAt(floor($player->getPosition()->getX()), floor($player->getPosition()->getZ()));
-          if($difference - 2 > $maxY){
-            $report->alert("Fly", $player->getName());
-        }
-        }
+      $Oldy = $event->getFrom()->getY();
+      $Newy = $event->getTo()->getY();
+      if($player->getAllowFlight() == false){
+          if($Oldy <= $Newy){
+              if($player->GetInAirTicks() > 20){
+                  $maxY = $player->getWorld()->getHighestBlockAt(floor($player->getPosition()->getX()), floor($player->getPosition()->getZ()));
+                  if($Newy - 2 > $maxY){
+                      $report->alert("Fly", $player->getName());
+                  }
+              }
+              
+          }
       }
     }
 }
