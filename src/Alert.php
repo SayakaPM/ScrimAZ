e210ab9d86acb5752af13cfd37e2faef455e3303
Sayaka <103137319+SayakaPM@users.noreply.github.com>
2022-06-11 11:16:01 +0100

    Update Alert.php

--- src/Alert.php
+++ src/Alert.php
@@ -10,17 +10,12 @@ use pocketmine\player\Player;
 use OnlyJaiden\ScrimAS\User;
 
 class Alert {
-  public $point = array();
   public function alert(string $cheat, string $player): void {
     $config = Main::getInstance()->getConfig();
     $user = new User;
     foreach(Server::getInstance()->getOnlinePlayers() as $staff) {
       if($staff->hasPermission("ScrimAS.alerts")) {
         $user->getUser($staff, $player, $cheat);
-        $this->point[$player][$cheat] += (float) 1;
-        if($this->point[$player][$cheat] == 5) {
-          // Todo kick player
-        }
       }     
     }  
   }
