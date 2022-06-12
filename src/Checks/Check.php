6991e586c8c0479eb6aac91637043e5ca214ee24
Sayaka <103137319+SayakaPM@users.noreply.github.com>
2022-06-13 00:48:07 +0100

    Block Below Player

--- src/Checks/Check.php
+++ src/Checks/Check.php
@@ -4,29 +4,36 @@ declare(strict_types=1);
 
 namespace OnlyJaiden\ScrimAS\Checks;
 
+// custom
 use OnlyJaiden\ScrimAS\User;
 use OnlyJaiden\ScrimAS\Main;
+use OnlyJaiden\ScrimAS\Alert;
+// pocketmine
 use pocketmine\Server;
+use pocketmine\math\Vector3;
 use pocketmine\player\Player;
 use pocketmine\entity\effect\VanillaEffects;
 use pocketmine\entity\effect\EffectInstance;
-use OnlyJaiden\ScrimAS\Alert;
+use pocketmine\block\Block;
 
 class Check {
   public function checkEffect($player, $cheat): void {
     $report = new Alert;
-    // Check if Player has LEVITATION as it alerts fly.
+    // Effects #0000
     if($cheat == 'Fly'){
         if($player->getEffects()->has(VanillaEffects::LEVITATION())){
             return;
         }
         $report->alert($cheat, $player->getName());
       }
-      // Check if Player has Speed as it alerts speed.
+      // Effects #0001
     if($cheat == 'Speed'){
       if($player->getEffects()->has(VanillaEffects::SPEED())){
           return;
       }
+      // Blocks #0002
+      $blockBelow = $player->getWorld()->getBlock(new Vector3($player->getPosition()->getX(), $player->getPosition()->getY() - 0.5, $player->getPosition()->getZ()));
+      $player->sendMessage($blockBelow);
       $report->alert($cheat, $player->getName());
   }
   }
